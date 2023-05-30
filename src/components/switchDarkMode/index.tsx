c2f0fd68bd7261e9fec507344fce98242188ed15
Theus <matheusth.dev@gmail.com>
2023-05-29 21:53:22 -0300

    refactor: mudança nas animações e mudança do som para um som mais limpo

--- src/components/switchDarkMode/index.tsx
+++ src/components/switchDarkMode/index.tsx
@@ -1,28 +1,30 @@
 import { useState } from "react";
 import * as T from "./types";
 import { FaSun, FaMoon } from "react-icons/fa";
-import slideSound from "../../assets/sounds/slide.mp3";
+import click1Sound from "../../assets/sounds/click1.wav";
+import click2Sound from "../../assets/sounds/click2.wav";
 
 export default function SwitchDarkMode({ switchAction }: T.ISwitchProps) {
   const [isActive, setIsActive] = useState(
     localStorage.getItem("@TDM-THEME") === "light" ? false : true
   );
 
-  const slide = new Audio(slideSound);
+  const click1 = new Audio(click1Sound);
+  const click2 = new Audio(click2Sound);
 
   return (
     <button
       onClick={() => {
         setIsActive(!isActive);
         switchAction();
-        slide.play();
+        isActive ? click2.play() : click1.play();
       }}
       className={`flex items-center w-10 h-5 transition duration-300 ${
         isActive ? "bg-white" : "bg-zinc-200"
-      } shadow-2xl md:w-10 md:h-5 rounded-2xl focus:outline-none`}
+      } shadow-inner md:w-10 md:h-5 rounded-2xl focus:outline-none`}
     >
       <div
-        className={`w-6 h-6 p-1 text-white transition duration-500 transform ${
+        className={`w-6 h-6 flex items-center justify-center p-1 text-white transition duration-500 transform ${
           isActive
             ? "bg-gray-700 translate-x-full"
             : "-translate-x-2 bg-yellow-500"
